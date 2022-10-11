1efccfd87b28ca9911d142fbdd17c6ead5e1d616
Mehedi Hasan <mehedihasan123456789.mh.mh@gmail.com>
2022-10-11 19:22:43 +0600

    custom msz added for other routs

--- src/App.js
+++ src/App.js
@@ -6,52 +6,64 @@ import Topics from './components/Topics/Topics';
 import Statistics from './components/Statistics/Statistics';
 import Blogs from './components/Blogs/Blogs';
 import QuizTopic from './components/QuizTopic/QuizTopic';
+import { ChevronDoubleDownIcon, EmojiSad, ExclamationTriangleIcon } from '@heroicons/react/24/solid'
 
 
-function App() {const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Main></Main>,
-    children:[
-      {
-        path: '/',
-        loader: ()=> fetch('https://openapi.programming-hero.com/api/quiz'),
-        element:<Topics></Topics>
-      },
-      {
-        path: '/topics',
-        loader: ()=> fetch('https://openapi.programming-hero.com/api/quiz'),
-        element:<Topics></Topics>
-      },
-      {
-        path: 'statistics',
-        element: <Statistics></Statistics>
-      },
-      {
-        path: 'blogs',
-        element: <Blogs></Blogs>
-      },
-      {
-        path: 'quiz/:quizid',
-        loader: async ({params})=> {
-          return fetch(`https://openapi.programming-hero.com/api/quiz/${params.quizid}`)
+
+function App() {
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Main></Main>,
+      children: [
+        {
+          path: '/',
+          loader: () => fetch('https://openapi.programming-hero.com/api/quiz'),
+          element: <Topics></Topics>
         },
-        element:<QuizTopic></QuizTopic>
-        
-      },
-  
-  
-    ],
-    
-  }
+        {
+          path: '/topics',
+          loader: () => fetch('https://openapi.programming-hero.com/api/quiz'),
+          element: <Topics></Topics>
+        },
+        {
+          path: 'statistics',
+          element: <Statistics></Statistics>
+        },
+        {
+          path: 'blogs',
+          element: <Blogs></Blogs>
+        },
+        {
+          path: 'quiz/:quizid',
+          loader: async ({ params }) => {
+            return fetch(`https://openapi.programming-hero.com/api/quiz/${params.quizid}`)
+          },
+          element: <QuizTopic></QuizTopic>
 
-  
-])
-return (
-  <div className='app-container '>
-    <RouterProvider router={router}></RouterProvider>
-  </div>
-);
+        },
+
+
+      ],
+
+    },
+    {
+      path: '*',
+      element: <div className='text-6xl bg-white text-center mt-52 text-orange-900 font-black'>
+        <h1>No Route Found!</h1>
+
+        <ExclamationTriangleIcon className='h-32 w-32 text-center mx-auto'></ExclamationTriangleIcon>
+
+      </div>
+    }
+
+
+  ])
+  return (
+    <div className='app-container '>
+      <RouterProvider router={router}></RouterProvider>
+    </div>
+  );
 }
 
 export default App;
